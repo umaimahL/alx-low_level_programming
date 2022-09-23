7b4ec37d76589496064dcfbc07167fdb38ccf86f
umaimahL <Maryamahmadshay@gmail.com>
2022-09-23 14:27:41 -0700

    C programming commit 2

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -printing
+ *
+ * return : always 0 (successful)
+ *
+ */
+int main(void)
+{
+	puts( "\" programming is like building a multilingual puzzle");
+	return(0);
+}
+
