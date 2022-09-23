b9bfb782d9922f4cf5173acd15f9e07867c97b79
umaimahL <Maryamahmadshay@gmail.com>
2022-09-23 15:07:12 -0700

    C programming commit 3

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a text
+ *
+ * return: Always 0(successful)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
