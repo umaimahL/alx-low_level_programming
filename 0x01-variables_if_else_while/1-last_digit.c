9b3cdd067b0675832609e0f862e9276343448844
umaimahL <Maryamahmadshay@gmail.com>
2022-09-24 07:30:55 -0700

    C programming commit 3

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- prints random number to variable
+ *
+ *Description: c program to print random number to variable
+ *
+ * Return: Always (successful)
+ */
+int main(void)
+
+{
+	int n;
+	int L;
+	char str[] = "last digit of";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	L = n % 10;
+
+	if (L > 5)
+	{
+		printf("%s %d is %d and is greater than 5\n", str, n, L);
+	}
+	else if (L == 0)
+	{
+		printf("%s %d is %d and is 0\n", str, n, L);
+	}
+	else if (L < 6)
+	{
+		printf("%s %d is %d is less than 6 and not 0\n", str, n, L);
+	}
+	return (0);
+}
+
+
