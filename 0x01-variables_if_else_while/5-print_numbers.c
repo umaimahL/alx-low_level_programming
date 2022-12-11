14034c623536990716804d402cace897e2ad0364
root <maryamahmadshay@gmail.com>
2022-12-10 16:31:33 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ *main - prints the numbers from 0-9
+ * 
+ *Return: Always (successful)
+ */
+int main(void)
+{
+	char n;
+	for (n = '0'; n<= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return(0);
+}
